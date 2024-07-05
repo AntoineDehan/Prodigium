9aea06be34709bb555003caea5b88a1406b0545a
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2024-07-05 14:42:37 +0200

    Added all basic items

--- src/main/java/net/denlille/prodigium/item/ModItems.java
+++ src/main/java/net/denlille/prodigium/item/ModItems.java
@@ -13,12 +13,48 @@ public class ModItems {
 
     public static final RegistryObject<Item>  TOKEN_MELEE = ITEMS.register("token_melee",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  TOKEN_MAGE = ITEMS.register("token_mage",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  TOKEN_RANGED = ITEMS.register("token_ranged",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  RESET_TOKEN_MELEE = ITEMS.register("reset_token_melee",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  RESET_TOKEN_MAGE = ITEMS.register("reset_token_mage",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  RESET_TOKEN_RANGED = ITEMS.register("reset_token_ranged",
+            () -> new Item(new Item.Properties()));
+
+
 
     public static final RegistryObject<Item>  RAW_COBALT = ITEMS.register("raw_cobalt",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item>  COBALT_INGOT = ITEMS.register("cobalt_ingot",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item>  COBALT_NUGGET = ITEMS.register("cobalt_nugget",
+            () -> new Item(new Item.Properties()));
+
+
+    public static final RegistryObject<Item>  RAW_PALLADIUM = ITEMS.register("raw_palladium",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  PALLADIUM_INGOT = ITEMS.register("palladium_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  PALLADIUM_NUGGET = ITEMS.register("palladium_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  PALLADIUM_ROD = ITEMS.register("palladium_rod",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item>  RAW_ORICHALCUM = ITEMS.register("raw_orichalcum",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  ORICHALCUM_INGOT = ITEMS.register("orichalcum_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  ORICHALCUM_NUGGET = ITEMS.register("orichalcum_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  ORICHALCUM_ROD = ITEMS.register("orichalcum_rod",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item>  SHROOMITE_INGOT = ITEMS.register("shroomite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>  SHROOMITE_ROD = ITEMS.register("shroomite_rod",
             () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
